a12f69c89305b25e422c4b61ee27390db44d3658
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-06-21 18:58:22 +0300

    Beautify сообщения об отключении устройства

--- internal/server/messages.go
+++ internal/server/messages.go
@@ -18,7 +18,7 @@ func getDeviceDisconnectedMessage(deviceID string) comm.OutgoingMessage {
 	return comm.OutgoingMessage{
 		DeviceID: deviceID,
 		Message: &communication.BotMessage{
-			Text: "Связь с устройством потеряна. Возможные причины:\n\t-устройство отключено;\n\t-потеря соединения с Интернет\n\t-отсутствие электричества\n\t-программный сбой;\n\t-обновление системы.",
+			Text: "Связь с устройством потеряна. Возможные причины:\n\t- устройство отключено;\n\t- потеря соединения с Интернет;\n\t- отсутствие электричества;\n\t- программный сбой;\n\t- обновление системы.",
 		},
 	}
 }
