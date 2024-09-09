deba3e5c71b153aade63adeaa8d07e251f6ef8e1
Yury Zaytsev <mrenotenot@gmail.com>
2024-09-09 12:37:31 +0300

    server: add extra logging

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -3,11 +3,12 @@ package server
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"time"
+
 	rms_users "github.com/RacoonMediaServer/rms-packages/pkg/service/rms-users"
 	"github.com/gorilla/websocket"
 	"go-micro.dev/v4/logger"
-	"net/http"
-	"time"
 )
 
 var upgrader = websocket.Upgrader{
@@ -34,6 +35,9 @@ func (s *Server) authorize(ctx context.Context, token string) (bool, error) {
 
 func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
 	l := s.l.Fields(map[string]interface{}{"addr": r.RemoteAddr})
+	for key, val := range r.Header {
+		l.Logf(logger.DebugLevel, "Got header %s = %+v", key, val)
+	}
 	token := r.Header.Get("X-Token")
 	if ok, err := s.authorize(r.Context(), token); !ok {
 		if err != nil {
@@ -48,6 +52,7 @@ func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
+		l.Logf(logger.ErrorLevel, "Upgrade connection failed: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
